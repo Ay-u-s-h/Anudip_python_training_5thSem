3f552a1233ed6a13ff7abb3b84c3ceb5a7247655
Ay-u-s-h <ayushgupta42r@gmail.com>
2026-06-04 23:31:01 +0530

    Update conssecutive_digit_number.py

--- self_learning/iteration_4thJune/conssecutive_digit_number.py
+++ self_learning/iteration_4thJune/conssecutive_digit_number.py
@@ -1,1 +1,16 @@
+# Program: Consecutive Digit Number
+# Task: Check if every digit is exactly 1 greater than the previous digit.
+
+num = input("Enter a number: ")
+is_consecutive = True
+
+for i in range(len(num) - 1):
+    if int(num[i+1]) - int(num[i]) != 1:
+        is_consecutive = False
+        break
+
+if is_consecutive:
+    print("Consecutive Number")
+else:
+    print("Not a Consecutive Number")
 
