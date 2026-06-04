34784a666c7fa15a00ea41f53a701f43f0b33e95
Ay-u-s-h <ayushgupta42r@gmail.com>
2026-06-04 23:19:16 +0530

    Update prime_number_analysis.py

--- labwork/iteration_4thJune_lab/prime_number_analysis.py
+++ labwork/iteration_4thJune_lab/prime_number_analysis.py
@@ -1,1 +1,25 @@
+# Program: Prime Number Analyser
+# Task: Accept a number, check if prime. If not, display its factors.
+
+num = int(input("Enter a number: "))
+
+# Flag to check prime status
+is_prime = True
+
+# Collect factors
+factors = []
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        factors.append(i)
+        # If divisible by something other than 1 and itself, not prime
+        if i != 1 and i != num:
+            is_prime = False
+
+# Display results
+print("Factors:", *factors)
+if is_prime:
+    print(f"{num} is a Prime Number")
+else:
+    print(f"{num} is not a Prime Number")
 
